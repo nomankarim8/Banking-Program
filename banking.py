c34c7dbea7ecd5376926a96123c6b1c50bef2888
nomankarim8 <karimnoman97@gmail.com>
2024-07-08 23:48:53 -0700

    update banking.py

--- banking.py
+++ banking.py
@@ -51,6 +51,7 @@ def main():
         print("*********************")
         choice = input("Enter your choice (1-4): ")
 
+
         if choice == '1':
             show_balance(balance)
         elif choice == '2':
